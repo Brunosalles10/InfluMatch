d257bdce6afc13c798d339585060a480867d77f2
Bruno Salles <bsalles428@gmail.com>
2026-05-09 12:47:55 -0300

    feat: integrate Prisma with NestJS and implement user management
    - Added Prisma configuration and service for database interactions.
    - Created initial migration for users table with necessary fields.
    - Implemented DTOs for user creation and updating with validation.
    - Set up Users module, controller, and service for user management.
    - Removed unused AppController and AppService.
    - Enhanced main application with global validation pipes.
    - Updated package dependencies for Prisma and NestJS.

---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
+  @MaxLength(50, { message: 'Nome deve conter no máximo 50 caracteres' })
+  @MinLength(3, { message: 'Nome deve conter no mínimo 3 caracteres' })
+  name: string;
+
+  @IsEmail({ message: 'Email inválido' })
+  @IsNotEmpty({ message: 'Email não pode ser vazio' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Senha não pode ser vazia' })
+  @MinLength(8, { message: 'Senha deve conter no mínimo 8 caracteres' })
+  @MaxLength(20, { message: 'Senha deve conter no máximo 20 caracteres' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/,
+    {
+      message:
+        'Senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial',
+    },
+  )
+  password: string;
+}
